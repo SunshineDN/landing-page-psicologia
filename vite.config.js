e26dc7a163ac1b6f93ac3ccdec228c495743c116
SunshineDN <sunshinedn2003@gmail.com>
2023-07-08 17:22:26 -0300

    adicionado chunkLimite personalizado

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,7 @@ import svgr from 'vite-plugin-svgr';
 export default defineConfig({
   plugins: [react(), svgr()],
   base: '/landing-page-psicologia/',
+  build: {
+    chunkSizeWarningLimit: 1600,
+  }
 });
